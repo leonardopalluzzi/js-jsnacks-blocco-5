e67518d6c7f14deb9b7939afd9dcfa612c99a5d3
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-26 16:05:20 +0100

    snack 1 complete

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names);
+
+for(let i = 0; i < names.length; i++){
+    const thisName = names[i];
+    console.log(thisName);   
+}
