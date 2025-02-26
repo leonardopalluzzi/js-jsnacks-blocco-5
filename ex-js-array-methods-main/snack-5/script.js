114e18bdba03cae0731a7620ecb45948100a6956
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-26 16:19:22 +0100

    snack 5 complete

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,19 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenArr = [];
+let j = 0;
+for(let i = 0; i < nums.length; i++){
+    const thisEl = nums[i]
+    if(thisEl % 2 == 0){
+        evenArr[j] = thisEl
+        j++
+    }
+}
+
+console.log(evenArr);
+
+
+const evenArrFilter = nums.filter(numb => numb % 2 == 0);
+console.log(evenArrFilter);
