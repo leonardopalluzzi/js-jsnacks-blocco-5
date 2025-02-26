99c7426166fe357c6b60e6fb07173b76668588db
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-26 16:26:59 +0100

    snack 7 complete

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,17 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let wantedStudent;
+for(let  i = 0; i < students.length; i++){
+  const thisEl = students[i];
+  if(thisEl.id == 2){
+    wantedStudent = thisEl;
+    break
+  }
+}
+console.log(wantedStudent);
+
+
+const wantedStudentFind = students.find(student => student.id == 2);
+console.log(wantedStudentFind);
