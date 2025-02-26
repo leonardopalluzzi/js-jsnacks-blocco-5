1c46ef1a6f5b691c20e1ecf574798df28f443026
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-26 16:11:36 +0100

    snack 3 complete

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,15 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const newArr = [];
+for(let i = 0; i < numbers.length; i++){
+    const thisEl = numbers[i];
+    newArr[i] = thisEl + 1
+}
+
+console.log(newArr);
+
+const newArrMap = numbers.map(numb => (numb + 1));
+console.log(newArrMap);
+
